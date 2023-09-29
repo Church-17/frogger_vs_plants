06709200a86277bd06d04bff91b91b9cf1aa32cf
Church-17 <matteo.chiesa.mc@gmail.com>
2023-09-29 10:46:01 +0200

    comment and some function

--- versione_thread/main.c
+++ versione_thread/main.c
@@ -1,3 +1,4 @@
+// Include libs
 #include <curses.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,24 +6,36 @@
 #include <time.h>
 #include <string.h>
 
-int xBeg, yBeg, xMax, yMax;
+// Define constant
 
+// Define type and struct
 typedef struct {
-    int x;
     int y;
+    int x;
 } Position;
 
+// Define global vars
+Position scr_beg, scr_max;
+
+// Prototypes
+void init_scr();
+
+// Main
 int main(void) {
 
+    init_scr(); // Initialize screen
+    pthread_t t1, t2, t3; // Initilize threads
+
+
+    endwin(); // End window
+    return 0;
+}
+
+// Inizialize screen & fetch screen info
+void init_scr() {
     initscr();
     noecho();
-    curs_set(0);
-
-    getbegyx(stdscr, yBeg, xBeg);
-    getmaxyx(stdscr, yMax, xMax);
-
-    pthread_t t1, t2, t3;
-
-
-    return 0;
+    curs_set(0); //Remove cursor
+    getbegyx(stdscr, scr_beg.y, scr_beg.x); //Get screen dimension
+    getmaxyx(stdscr, scr_max.y, scr_max.x);
 }
