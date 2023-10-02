9cfc5a4eca9ffb94efa0792f3e53cdb4b8eda52c
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-02 12:29:54 +0200

    remove init_scr func

--- versione_thread/main.c
+++ versione_thread/main.c
@@ -15,27 +15,21 @@ typedef struct {
 } Position;
 
 // Define global vars
-Position scr_beg, scr_max;
 
 // Prototypes
-void init_scr();
 
 // Main
 int main(void) {
+    Position scr_beg, scr_max;
+    initscr(); // Initialize window
+    noecho();
+    curs_set(0); //Remove cursor
+    getbegyx(stdscr, scr_beg.y, scr_beg.x);
+    getmaxyx(stdscr, scr_max.y, scr_max.x); //Get screen dimension
 
-    init_scr(); // Initialize screen
     pthread_t t1, t2, t3; // Initilize threads
 
 
     endwin(); // End window
     return 0;
-}
-
-// Inizialize screen & fetch screen info
-void init_scr() {
-    initscr();
-    noecho();
-    curs_set(0); //Remove cursor
-    getbegyx(stdscr, scr_beg.y, scr_beg.x); //Get screen dimension
-    getmaxyx(stdscr, scr_max.y, scr_max.x);
 }
