aa92a897b0166d9d43eb1c6831573a698d5372f8
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-04 11:29:02 +0200

    comment

--- versione_thread/utils.h
+++ versione_thread/utils.h
@@ -6,10 +6,10 @@
 
 // Define macros
 #define mod(x, y) ((x%y + y) % y)
-#define wattrprintw(win, attr, str, args...) wattron(win, attr); wprintw(win, str, args); wattroff(win, attr) // prints a string enabling an attribute
-#define mvwattrprintw(win, row, col, attr, str, args...) wmove(win, row, col); wattrprintw(win, attr, str, args) // printsa a string whit an attribute in a position
-#define wctrprintw(win, row, str) mvwprintw(win, row, (win->_maxx - strlen(str))/2, "%s", str) // centers a string
-#define wctrattrprintw(win, row, attr, str) wmvattrprintw(win, row, (win->_maxx - strlen(str))/2, attr, "%s", str) // prints a centered string with an attribute
+#define wattrprintw(win, attr, str, args...) wattron(win, attr); wprintw(win, str, args); wattroff(win, attr) // Print string with attribute
+#define mvwattrprintw(win, row, col, attr, str, args...) wmove(win, row, col); wattrprintw(win, attr, str, args) // Move & print string with attribute
+#define wctrprintw(win, row, str) mvwprintw(win, row, (win->_maxx - strlen(str))/2, "%s", str) // Print string centered
+#define wctrattrprintw(win, row, attr, str) wmvattrprintw(win, row, (win->_maxx - strlen(str))/2, attr, "%s", str) // Prints string centered with attribute
 
 // Define type and struct
 typedef struct {
