881b74daf917b9844563f8baa3f98b210c6495b1
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-06 23:35:23 +0200

    settings menu

--- versione_thread/menu.c
+++ versione_thread/menu.c
@@ -1,4 +1,6 @@
 // Include libs
+#include <stdlib.h>
+#include <stdio.h>
 #include <curses.h>
 #include <string.h>
 #include "menu.h"
@@ -77,6 +79,109 @@ int menu(str title, List_str opts, bool nav) {
     }
 }
 
+int double_menu(str title, List_str* opts, int* setted, int opts_len, bool nav) {
+    int i, max_optlen = strlen(title);
+    for(i = 0; i < opts_len; i++) {
+        max_optlen = max_strlen(opts[i], max_optlen);
+    }
+    max_optlen *= 2;
+    printf("%d", max_optlen);
+
+    WINDOW* menu_win = newwin(opts_len+BOX_PADN+BOX_PADS+2, max_optlen+BOX_PADE+BOX_PADW, (LINES - (opts_len+BOX_PADN+BOX_PADS+2))/2, (COLS - (max_optlen+BOX_PADE+BOX_PADW))/2);
+    keypad(menu_win, TRUE); // Enable function keys listener
+    box(menu_win, 0, 0); // Box the window
+    wctrprintw(menu_win, 0, title); // Print title
+
+    if(nav) {
+        int key, hl = 0, old_hl = 0;
+        for (i = 0; i < opts_len; i++) { // Print other option
+            mvwattrprintw(menu_win, i+BOX_PADN, BOX_PADW, A_UNDERLINE, "%c", opts[i].list[0][0]);
+            wprintw(menu_win, "%s", &(opts[i].list[0][1]));
+            mvwprintw(menu_win, i+BOX_PADN, max_optlen+BOX_PADW+BOX_PADE-2-strlen(opts[i].list[setted[i]]), "%s", opts[i].list[setted[i]]);
+        }
+        mvwattrprintw(menu_win, i+BOX_PADN+1, BOX_PADW, A_UNDERLINE, "%c", 'B');
+        wprintw(menu_win, "%s", "ack");
+        
+        while(TRUE) {
+            // Update highlighted & non-highlighted option
+            if(old_hl == opts_len) {
+                mvwattrprintw(menu_win, old_hl+BOX_PADN+1, BOX_PADW, A_UNDERLINE, "%c", 'B');
+                wprintw(menu_win, "ack ");
+            } else {
+                mvwattrprintw(menu_win, old_hl+BOX_PADN, BOX_PADW, A_UNDERLINE, "%c", opts[old_hl].list[0][0]);
+                wprintw(menu_win, "%s", &(opts[old_hl].list[0][1]));
+                mvwprintw(menu_win, old_hl+BOX_PADN, max_optlen+BOX_PADW+BOX_PADE-2-strlen(opts[old_hl].list[setted[old_hl]]), "%s", opts[old_hl].list[setted[old_hl]]);
+            }
+            if(hl == opts_len) {
+                mvwprintw(menu_win, hl+BOX_PADN+1, BOX_PADW, " ");
+                wattrprintw(menu_win, A_STANDOUT, "%s", "Back");
+            } else {
+                mvwattrprintw(menu_win, hl+BOX_PADN, BOX_PADW, A_STANDOUT, "%s", opts[hl].list[0]);
+                mvwattrprintw(menu_win, hl+BOX_PADN, max_optlen+BOX_PADW+BOX_PADE-2-strlen(opts[hl].list[setted[hl]]), A_STANDOUT, "%s", opts[hl].list[setted[hl]]);
+            }
+
+            old_hl = hl;
+
+            key = wgetch(menu_win);
+            switch (key) {
+                case KEY_UP:
+                case KEY_PPAGE:
+                    if (--hl < 0) {
+                        hl = opts_len;
+                    }
+                    break;
+                
+                case KEY_DOWN:
+                case KEY_NPAGE:
+                    if (++hl > opts_len) {
+                        hl = 0;
+                    }
+                    break;
+
+                case KEY_LEFT:
+                    mvwprintw(menu_win, hl+BOX_PADN, max_optlen+BOX_PADW+BOX_PADE-2-strlen(opts[hl].list[setted[hl]]), "%*s", (int) strlen(opts[hl].list[setted[hl]]), "");
+                    if(--setted[hl] <= 0) {
+                        setted[hl] = opts[hl].len - 1;
+                    }
+                    break;
+
+                case KEY_RIGHT:
+                    mvwprintw(menu_win, hl+BOX_PADN, max_optlen+BOX_PADW+BOX_PADE-2-strlen(opts[hl].list[setted[hl]]), "%*s", (int) strlen(opts[hl].list[setted[hl]]), "");
+                    if(++setted[hl] >= opts[hl].len) {
+                        setted[hl] = 1;
+                    }
+                    break;
+
+                case KEY_HOME:
+                    hl = 0;
+                    break;
+
+                case KEY_END:
+                case 'B':
+                case 'b':
+                    hl = opts_len;
+                    break;
+
+                case ENTER:
+                    if(hl == opts_len) {
+                        unwin(menu_win);
+                        return hl;
+                    }
+                    break;
+
+                default:
+                    for(i = 0; i < opts_len; i++) {
+                        if(key == opts[i].list[0][0] || key == opts[i].list[0][0]+32) {
+                            hl = i;
+                            break;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
+
 // Home Menu function
 void home_menu() {
     int chosen;
@@ -94,7 +199,7 @@ void home_menu() {
                 best_scores();
                 break;
             case 2:
-                game_settings();
+                settings();
                 break;
             case 3:
                 credits_menu();
@@ -111,8 +216,16 @@ void best_scores() {
 }
 
 // Game settings menu
-void game_settings() {
-
+void settings() {
+    str set1[4] = {"Difficulty", "Easy", "Normal", "Difficult"};
+    str set2[4] = {"Skin", "Common", "Rare", "Epic"};
+    List_str opts[2];
+    opts[0].list = set1;
+    opts[0].len = 4;
+    opts[1].list = set2;
+    opts[1].len = 4;
+    int setted[2] = {2, 3};
+    double_menu(" Settings ", opts, setted, 2, TRUE);
 }
 
 // Credits screen
--- versione_thread/menu.h
+++ versione_thread/menu.h
@@ -3,21 +3,22 @@
 
 // Define constants
 #define TITLE " PAC-MAN "
+#define CREDITS " Credits "
+#define PAUSE " Pause "
 #define BOX_PADN 2 // North padding of the box
 #define BOX_PADE 5 // East padding of the box
 #define BOX_PADW 2 // West padding of the box
 #define BOX_PADS 1 // South padding of the box
-#define CREDITS " Credits "
-#define PAUSE " Pause "
 #define N_OPTS_HOME 5
 #define N_OPTS_CREDS 4
 #define N_OPTS_PAUSE 5
 
 // Prototypes
 int menu(str title, List_str opts, bool nav);
+int double_menu(str title, List_str* opts, int* setted, int opts_len, bool nav);
 void home_menu(void);
 void best_scores(void);
-void game_settings(void);
+void settings(void);
 void credits_menu(void);
 int pause_menu(void);
 void endgame_menu(int score);
