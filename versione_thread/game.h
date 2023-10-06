4e4eff2ed3a2d298221b002fb5eb839e46b84733
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-06 10:04:56 +0200

    init game.h

--- versione_thread/game.h
+++ versione_thread/game.h
@@ -1,18 +1,10 @@
 // Include libs
 #include <curses.h>
 #include <string.h>
-#include "utils.h"
 #include "menu.h"
 
-int pause_menu() {
-    str list[5] = {"Resume", "New game", "Settings", "Menu", "Quit"};
-    List_str opts;
-    opts.list = list;
-    opts.len = 5;
-    return menu("Pause", opts, TRUE);
-}
+// Define constant
 
-int game() {
-
-
+void game() {
+    getch();
 }
