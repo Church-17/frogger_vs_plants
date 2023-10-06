df6b88a1c878e4ec2535abe83bb77c1fc6e728e7
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-06 10:05:06 +0200

    better utils

--- versione_thread/utils.h
+++ versione_thread/utils.h
@@ -5,14 +5,20 @@
 #define ENTER 10
 
 // Define macros
+
+// - Math
 #define mod(x, y) ((x%y + y) % y)
+
+// - Print
 #define wattrprintw(win, attr, fs, args...) wattron(win, attr); wprintw(win, fs, args); wattroff(win, attr) // Print string with attribute
 #define mvwattrprintw(win, row, col, attr, fs, args...) wmove(win, row, col); wattrprintw(win, attr, fs, args) // Move & print string with attribute
 #define wctrprintw(win, row, fs) mvwprintw(win, row, (win->_maxx - strlen(fs))/2, "%s", fs) // Print string centered
 #define wctrattrprintw(win, row, attr, fs) wmvattrprintw(win, row, (win->_maxx - strlen(fs))/2, attr, "%s", fs) // Prints string centered with attribute
+
+// - Delete window
 #define unwin(win) werase(win); wrefresh(win); delwin(win); clear(); refresh()
 
-// Define type and struct
+// Define types and structs
 typedef char* str;
 
 typedef struct {
@@ -24,3 +30,16 @@ typedef struct {
     str* list;
     int len;
 } List_str;
+
+// Define functions
+
+// - Find max length of a strings list
+int max_strlen(List_str strings, int min_len) {
+    int max_len = min_len;
+    for(int i = 0; i < strings.len; i++) {
+        if(strlen(strings.list[i]) > max_len) {
+            max_len = strlen(strings.list[i]);
+        }
+    }
+    return max_len;
+}
