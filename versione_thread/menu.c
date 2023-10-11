212e11e41417aafd8923458d3a7855c5a7ec9ee2
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-11 12:49:03 +0200

    change var

--- versione_thread/menu.c
+++ versione_thread/menu.c
@@ -6,9 +6,9 @@
 #include "utils.h"
 #include "strings.h"
 
-int menu(str title, List_str sets, bool nav) {
-    int i, win_width = max_strlen(sets, strlen(title)) + BOX_PADW + BOX_PADE; // Calc window width
-    WINDOW* menu_win = newctrwin(sets.len+BOX_PADN+BOX_PADS, win_width); // Centered win
+int menu(str title, List_str set, bool nav) {
+    int i, win_width = max_strlen(set, strlen(title)) + BOX_PADW + BOX_PADE; // Calc window width
+    WINDOW* menu_win = newctrwin(set.len+BOX_PADN+BOX_PADS, win_width); // Centered win
     keypad(menu_win, TRUE); // Enable function keys listener
     box(menu_win, 0, 0); // Box win
     wctrprintw(menu_win, 0, title); // Print title
@@ -17,15 +17,15 @@ int menu(str title, List_str sets, bool nav) {
         int key, hl = 0, old_hl = 0; // Init vars
 
         // Print options with first letter underlined
-        for (i = 0; i < sets.len; i++) {
-            mvwfattrprintw(menu_win, i+BOX_PADN, BOX_PADW, A_UNDERLINE, sets.list[i]);
+        for (i = 0; i < set.len; i++) {
+            mvwfattrprintw(menu_win, i+BOX_PADN, BOX_PADW, A_UNDERLINE, set.list[i]);
         }
         while(TRUE) {
             // Update highlighted & non-highlighted option
-            mvwfattrprintw(menu_win, old_hl+BOX_PADN, BOX_PADW, A_UNDERLINE, sets.list[old_hl]);
+            mvwfattrprintw(menu_win, old_hl+BOX_PADN, BOX_PADW, A_UNDERLINE, set.list[old_hl]);
             wprintw(menu_win, "%*s", PAD_HL, "");
             mvwprintw(menu_win, hl+BOX_PADN, BOX_PADW, "%*s", PAD_HL, "");
-            wattrprintw(menu_win, A_STANDOUT, "%s", sets.list[hl]);
+            wattrprintw(menu_win, A_STANDOUT, "%s", set.list[hl]);
 
             old_hl = hl; // Track old hl
 
@@ -35,14 +35,14 @@ int menu(str title, List_str sets, bool nav) {
                 case KEY_LEFT:
                 case KEY_PPAGE:
                     if (--hl < 0) {
-                        hl = sets.len - 1;
+                        hl = set.len - 1;
                     }
                     break;
                 
                 case KEY_DOWN:
                 case KEY_RIGHT:
                 case KEY_NPAGE:
-                    if (++hl >= sets.len) {
+                    if (++hl >= set.len) {
                         hl = 0;
                     }
                     break;
@@ -52,7 +52,7 @@ int menu(str title, List_str sets, bool nav) {
                     break;
 
                 case KEY_END:
-                    hl = sets.len-1;
+                    hl = set.len-1;
                     break;
 
                 case ENTER:
@@ -61,8 +61,8 @@ int menu(str title, List_str sets, bool nav) {
 
                 default:
                     // Check first letters
-                    for(i = 0; i < sets.len; i++) {
-                        if(key == sets.list[i][0] || key == sets.list[i][0]+DIFF_CAPITAL) {
+                    for(i = 0; i < set.len; i++) {
+                        if(key == set.list[i][0] || key == set.list[i][0]+DIFF_CAPITAL) {
                             hl = i;
                             break;
                         }
@@ -72,8 +72,8 @@ int menu(str title, List_str sets, bool nav) {
         }
     } else {
         // Prints options
-        for (i = 0; i < sets.len; i++) {
-            mvwprintw(menu_win, i+BOX_PADN, BOX_PADW, "%s", sets.list[i]);
+        for (i = 0; i < set.len; i++) {
+            mvwprintw(menu_win, i+BOX_PADN, BOX_PADW, "%s", set.list[i]);
         }
         wgetch(menu_win);
         unwin(menu_win);
@@ -81,15 +81,15 @@ int menu(str title, List_str sets, bool nav) {
     }
 }
 
-int* double_menu(str title, List_str sets, List_str* opts, int* setted, bool nav) {
-    int i, win_width = max_strlen(sets, strlen(title));
+int* double_menu(str title, List_str set, List_str* opts, int* setted, bool nav) {
+    int i, win_width = max_strlen(set, strlen(title));
     // Calc window width
-    for(i = 0; i < sets.len; i++) {
+    for(i = 0; i < set.len; i++) {
         win_width = max_strlen(opts[i], win_width);
     }
     win_width += win_width + LR_ARROWS + BOX_PADW + BOX_PADE;
 
-    WINDOW* menu_win = newctrwin(sets.len+BOX_PADN+BOX_PADS+(2*nav), win_width); // 2*nav create space for last button if nav
+    WINDOW* menu_win = newctrwin(set.len+BOX_PADN+BOX_PADS+(2*nav), win_width); // 2*nav create space for last button if nav
     keypad(menu_win, TRUE); // Enable function keys listener
     box(menu_win, 0, 0); // Box the window
     wctrprintw(menu_win, 0, title); // Print title
@@ -97,30 +97,30 @@ int* double_menu(str title, List_str sets, List_str* opts, int* setted, bool nav
     if(nav) {
         int key, hl = 0, old_hl = 0;
         str last = "Back";
-        // Print sets and opts
-        for (i = 0; i < sets.len; i++) {
-            mvwfattrprintw(menu_win, i+BOX_PADN, BOX_PADW, A_UNDERLINE, sets.list[i]);
+        // Print set and opts
+        for (i = 0; i < set.len; i++) {
+            mvwfattrprintw(menu_win, i+BOX_PADN, BOX_PADW, A_UNDERLINE, set.list[i]);
             mvwprintw(menu_win, i+BOX_PADN, win_width-BOX_DIM-strlen(opts[i].list[setted[i]]), "%s", opts[i].list[setted[i]]);
         }
         // Print last button
-        mvwfattrprintw(menu_win, sets.len+BOX_PADN+1, BOX_PADW, A_UNDERLINE, last);
+        mvwfattrprintw(menu_win, set.len+BOX_PADN+1, BOX_PADW, A_UNDERLINE, last);
         
         while(TRUE) {
             // Update highlighted & non-highlighted option
-            if(old_hl == sets.len) {
+            if(old_hl == set.len) {
                 mvwfattrprintw(menu_win, old_hl+BOX_PADN+1, BOX_PADW, A_UNDERLINE, last);
                 wprintw(menu_win, "%*s", PAD_HL, "");
             } else {
-                mvwfattrprintw(menu_win, old_hl+BOX_PADN, BOX_PADW, A_UNDERLINE, sets.list[old_hl]);
+                mvwfattrprintw(menu_win, old_hl+BOX_PADN, BOX_PADW, A_UNDERLINE, set.list[old_hl]);
                 wprintw(menu_win, "%*s", PAD_HL, "");
                 mvwprintw(menu_win, old_hl+BOX_PADN, win_width-BOX_DIM-LR_ARROWS-strlen(opts[old_hl].list[setted[old_hl]]), "    %s", opts[old_hl].list[setted[old_hl]]);
             }
-            if(hl == sets.len) {
+            if(hl == set.len) {
                 mvwprintw(menu_win, hl+BOX_PADN+1, BOX_PADW, "%*s", PAD_HL, "");
                 wattrprintw(menu_win, A_STANDOUT, "%s", last);
             } else {
                 mvwprintw(menu_win, hl+BOX_PADN, BOX_PADW, "%*s", PAD_HL, "");
-                wattrprintw(menu_win, A_STANDOUT, "%s", sets.list[hl]);
+                wattrprintw(menu_win, A_STANDOUT, "%s", set.list[hl]);
                 mvwattrprintw(menu_win, hl+BOX_PADN, win_width-BOX_DIM-LR_ARROWS-strlen(opts[hl].list[setted[hl]]), A_STANDOUT, "◄ %s ►", opts[hl].list[setted[hl]]);
             }
 
@@ -131,13 +131,13 @@ int* double_menu(str title, List_str sets, List_str* opts, int* setted, bool nav
                 case KEY_UP:
                 case KEY_PPAGE:
                     if (--hl < 0) {
-                        hl = sets.len;
+                        hl = set.len;
                     }
                     break;
                 
                 case KEY_DOWN:
                 case KEY_NPAGE:
-                    if (++hl > sets.len) {
+                    if (++hl > set.len) {
                         hl = 0;
                     }
                     break;
@@ -161,11 +161,11 @@ int* double_menu(str title, List_str sets, List_str* opts, int* setted, bool nav
                     break;
 
                 case KEY_END:
-                    hl = sets.len;
+                    hl = set.len;
                     break;
 
                 case ENTER:
-                    if(hl == sets.len) {
+                    if(hl == set.len) {
                         unwin(menu_win);
                         return setted;
                     }
@@ -173,22 +173,22 @@ int* double_menu(str title, List_str sets, List_str* opts, int* setted, bool nav
 
                 default:
                     // Check first letters
-                    for(i = 0; i < sets.len; i++) {
-                        if(key == sets.list[i][0] || key == sets.list[i][0]+DIFF_CAPITAL) {
+                    for(i = 0; i < set.len; i++) {
+                        if(key == set.list[i][0] || key == set.list[i][0]+DIFF_CAPITAL) {
                             hl = i;
                             break;
                         }
                     }
                     if(key == last[0] || key == last[0]+DIFF_CAPITAL) {
-                        hl = sets.len;
+                        hl = set.len;
                     }
                     break;
             }
         }
     } else {
-        // Print sets & opts
-        for (i = 0; i < sets.len; i++) {
-            mvwprintw(menu_win, i+BOX_PADN, BOX_PADW, "%s", sets.list[i]);
+        // Print set & opts
+        for (i = 0; i < set.len; i++) {
+            mvwprintw(menu_win, i+BOX_PADN, BOX_PADW, "%s", set.list[i]);
             mvwprintw(menu_win, i+BOX_PADN, win_width+BOX_PADE-strlen(opts[i].list[setted[i]]), "%s", opts[i].list[setted[i]]);
         }
         wgetch(menu_win);
@@ -201,11 +201,11 @@ int* double_menu(str title, List_str sets, List_str* opts, int* setted, bool nav
 void home_menu(void) {
     int chosen;
     str list[N_OPTS_HOME] = {"New game", "Best scores", "Settings", "Credits", "Quit"};
-    List_str sets;
-    sets.list = list;
-    sets.len = N_OPTS_HOME;
+    List_str set;
+    set.list = list;
+    set.len = N_OPTS_HOME;
     while(TRUE) {
-        chosen = menu(TITLE, sets, TRUE);
+        chosen = menu(TITLE, set, TRUE);
         switch(chosen) {
             case 0:
                 game();
@@ -232,10 +232,10 @@ void best_scores() {
 
 // Game settings menu
 void settings() {
-    str set[2] = {"Difficulty", "Skin"};
-    List_str sets;
-    sets.list = set;
-    sets.len = 2;
+    str list[2] = {"Difficulty", "Skin"};
+    List_str set;
+    set.list = list;
+    set.len = 2;
     str opt1[3] = {"Easy", "Normal", "Difficult"};
     str opt2[3] = {"Common", "Rare", "Epic"};
     List_str opts[2];
@@ -244,24 +244,24 @@ void settings() {
     opts[1].list = opt2;
     opts[1].len = 3;
     int setted[2] = {1, 2};
-    double_menu(" Settings ", sets, opts, setted, TRUE);
+    double_menu(" Settings ", set, opts, setted, TRUE);
 }
 
 // Credits screen
 void credits_menu() {
     str list[N_OPTS_CREDS] = {"SOPR Project 23-24:", "", "Francesco Cardia", "Matteo Chiesa"};
-    List_str sets;
-    sets.list = list;
-    sets.len = N_OPTS_CREDS;
-    menu(" Credits ", sets, FALSE);
+    List_str set;
+    set.list = list;
+    set.len = N_OPTS_CREDS;
+    menu(" Credits ", set, FALSE);
 }
 
 int pause_menu() {
     str list[N_OPTS_PAUSE] = {"Resume", "New game", "Settings", "Home menu", "Quit"};
-    List_str sets;
-    sets.list = list;
-    sets.len = N_OPTS_PAUSE;
-    int chosen = menu(" Pause ", sets, TRUE);
+    List_str set;
+    set.list = list;
+    set.len = N_OPTS_PAUSE;
+    int chosen = menu(" Pause ", set, TRUE);
     switch(chosen) {
         default:
             break;
