92d0912733da123aadd0be8eebad5f7c1b1d9e77
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-15 16:09:59 +0200

    stupidaggini

--- versione_thread/menu.h
+++ versione_thread/menu.h
@@ -3,8 +3,8 @@
 
 // Define constants
 #define BOX_PADN 2 // North padding of the box
-#define BOX_PADE 5 // East padding of the box
 #define BOX_PADW 2 // West padding of the box
+#define BOX_PADE 5 // East padding of the box
 #define BOX_PADS 1 // South padding of the box
 #define HL_PADX 1 // Highlight padding
 #define SEL_PADY 1 // Empty lines in settings
