89470528672ce0f35c4680564401f097980c1c44
Church-17 <matteo.chiesa.mc@gmail.com>
2023-10-21 14:11:26 +0200

    bugfix & macros in source file

--- versione_thread/menu.c
+++ versione_thread/menu.c
@@ -6,11 +6,20 @@
 #include "utils.h"
 #include "resources.h"
 
+// Define constants
+#define BOX_PADN 2 // North padding of the box
+#define BOX_PADW 2 // West padding of the box
+#define BOX_PADE 5 // East padding of the box
+#define BOX_PADS 1 // South padding of the box
+#define HL_PADX 1 // Highlight padding
+#define SEL_PADY 1 // Empty lines in settings
+#define LR_ARROWS 4 // N chars occupied by arrows in settings
+
 void view(str title, List_str sx, List_str dx) {
     if(sx.len != dx.len) { // Check equal length
         return;
     }
-    int i, win_width = max_strlen(sx, strlen(title)) + max_strlen(dx, 0) + BOX_PADW + BOX_PADE; // Calc window width
+    int i, win_width = max(max_strlen(sx, 0)+max_strlen(dx, 0), strlen(title)) + BOX_PADW + BOX_PADE; // Calc window width
     WINDOW* menu_win = newctrwin(sx.len+BOX_PADN+BOX_PADS, win_width); // Centered window
     keypad(menu_win, TRUE); // Enable function keys listener
     box(menu_win, 0, 0); // Box window
@@ -76,7 +85,7 @@ int menu(str title, List_str set) {
                 return hl;
 
             default:
-                // Check first letters
+                // Check first letters from hl
                 for(i = (hl+1)%set.len; i != hl; i = (i+1)%set.len) {
                     if(key == set.list[i][0] || key == set.list[i][0]+DIFF_CAPITAL) {
                         hl = i;
@@ -98,14 +107,14 @@ void settings(void) {
     str difficulty[] = {DIFFICULTY_0, DIFFICULTY_1, DIFFICULTY_2};
     str skin[] = {SKIN_0, SKIN_1, SKIN_2};
 
-    List_str set;
+    List_str set; // Settings
     set.list = set0;
     set.len = N_SETTINGS;
-    List_str sel;
+    List_str sel; // Selectables
     sel.list = sel0;
     sel.len = N_SETTINGS_SEL;
 
-    List_str opts[N_SETTINGS];
+    List_str opts[N_SETTINGS]; // Options for each settings
     opts[0].list = language;
     opts[0].len = N_LANGUAGE;
     opts[1].list = difficulty;
@@ -113,7 +122,7 @@ void settings(void) {
     opts[2].list = skin;
     opts[2].len = N_SKIN;
 
-    int newly_setted[N_SETTINGS];
+    int newly_setted[N_SETTINGS]; // Settings before apply
     for(i = 0; i < N_SETTINGS; i++) {
         newly_setted[i] = game_params[i];
     }
@@ -125,17 +134,16 @@ void settings(void) {
     }
     win_width = set_width + opts_width + LR_ARROWS + BOX_PADW + BOX_PADE;
 
-    WINDOW* menu_win = newctrwin(set.len+sel.len+BOX_PADN+BOX_PADS+SEL_PADY, win_width); // Centered window
+    WINDOW* menu_win = newctrwin(set.len+sel.len+BOX_PADN+BOX_PADS+SEL_PADY, win_width); // Create centered window
     keypad(menu_win, TRUE); // Enable function keys listener
     box(menu_win, 0, 0); // Box window
     wctrprintw(menu_win, 0, SETTINGS); // Print title
 
-    // Print set, opts, selectables
-    for(i = 0; i < set.len; i++) {
+    for(i = 0; i < set.len; i++) { // Print sets, opts
         mvwfattrprintw(menu_win, i+BOX_PADN, BOX_PADW, A_UNDERLINE, set.list[i]);
         mvwprintw(menu_win, i+BOX_PADN, win_width-BOX_PADW-strlen(opts[i].list[newly_setted[i]]), "%s", opts[i].list[newly_setted[i]]);
     }
-    for(i = 0; i < sel.len; i++) {
+    for(i = 0; i < sel.len; i++) { // Print sels
         mvwfattrprintw(menu_win, i+set.len+BOX_PADN+SEL_PADY, BOX_PADW, A_UNDERLINE, sel.list[i]);
     }
     
@@ -177,20 +185,20 @@ void settings(void) {
                 break;
 
             case KEY_LEFT:
-                if(hl < set.len) { // If hl is a selectable delete old option highlighted
+                if(hl < set.len) { // If hl is a settings delete old option highlighted
                     mvwprintw(menu_win, hl+BOX_PADN, win_width-BOX_PADW-LR_ARROWS-strlen(opts[hl].list[newly_setted[hl]]), "%*s", (int) strlen(opts[hl].list[newly_setted[hl]])+LR_ARROWS, "");
-                }
-                if(--newly_setted[hl] < 0) {
-                    newly_setted[hl] = opts[hl].len - 1;
+                    if(--newly_setted[hl] < 0) {
+                        newly_setted[hl] = opts[hl].len - 1;
+                    }
                 }
                 break;
 
             case KEY_RIGHT:
-                if(hl < set.len) { // If hl is a selectable delete old option highlighted
+                if(hl < set.len) { // If hl is a settings delete old option highlighted
                     mvwprintw(menu_win, hl+BOX_PADN, win_width-BOX_PADW-LR_ARROWS-strlen(opts[hl].list[newly_setted[hl]]), "%*s", (int) strlen(opts[hl].list[newly_setted[hl]])+LR_ARROWS, "");
-                }
-                if(++newly_setted[hl] >= opts[hl].len) {
-                    newly_setted[hl] = 0;
+                    if(++newly_setted[hl] >= opts[hl].len) {
+                        newly_setted[hl] = 0;
+                    }
                 }
                 break;
 
@@ -203,7 +211,7 @@ void settings(void) {
                 break;
 
             case ENTER:
-                if(hl >= set.len) {
+                if(hl >= set.len) { // If hl is a selectable...
                     if(hl == set.len) {
                         for(i = 0; i < N_SETTINGS; i++) {
                             game_params[i] = newly_setted[i];
@@ -213,9 +221,10 @@ void settings(void) {
                     unwin(menu_win);
                     return;
                 }
+                break;
 
             default:
-                // Check first letters
+                // Check first letters from hl
                 for(i = (hl+1)%(set.len+sel.len); i != hl; i = (i+1)%(set.len+sel.len)) {
                     if(i < set.len) {
                         if(key == set.list[i][0] || key == set.list[i][0]+DIFF_CAPITAL) {
@@ -268,27 +277,27 @@ void home_menu(void) {
 // Best scores screen
 void best_scores(void) {
     int i;
-    UserScore* best = rd_best();
-    dalloc(str, users, N_BEST);
-    dalloc(str, scores, N_BEST);
+    UserScore* best = rd_best(); // Retreive best scores
+    str users[N_BEST], scores[N_BEST];
     List_str sx, dx;
     sx.list = users;
     dx.list = scores;
-    sx.len = N_BEST;
-    dx.len = N_BEST;
+    sx.len = dx.len = N_BEST;
     for(i = 0; i < N_BEST; i++) {
         if(best[i].score >= 0) {
-            sx.list[i] = best->user;
+            sx.list[i] = best[i].user;
             dx.list[i] = int_to_str(best[i].score);
         } else {
-            sx.list[i] = alloc(char, 10);
-            dx.list[i] = alloc(char, 10);
-            sx.list[i] = "";
-            dx.list[i] = "";
+            sx.list[i] = dx.list[i] = "";
         }
     }
     view(BEST_SCORES, sx, dx);
-    // FREE MEMORY
+
+    // Free memory
+    for(i = 0; i < N_BEST; i++) {
+        free(best[i].user);
+    }
+    free(best);
 }
 
 // Credits screen
@@ -297,9 +306,8 @@ void credits(void) {
     str list1[] = {"", "", "", ""};
     List_str sx, dx;
     sx.list = list0;
-    sx.len = N_CREDITS;
     dx.list = list1;
-    dx.len = N_CREDITS;
+    sx.len = dx.len = N_CREDITS;
     view(CREDITS, sx, dx);
 }
 
--- versione_thread/menu.h
+++ versione_thread/menu.h
@@ -1,15 +1,6 @@
 // Include libs
 #include "struct.h"
 
-// Define constants
-#define BOX_PADN 2 // North padding of the box
-#define BOX_PADW 2 // West padding of the box
-#define BOX_PADE 5 // East padding of the box
-#define BOX_PADS 1 // South padding of the box
-#define HL_PADX 1 // Highlight padding
-#define SEL_PADY 1 // Empty lines in settings
-#define LR_ARROWS 4 // N chars occupied by arrows in settings
-
 // Prototypes
 void view(str title, List_str sx, List_str dx);
 int menu(str title, List_str opts);
