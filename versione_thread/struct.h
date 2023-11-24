4d4bc751a8085ce565b12cb2b9486572702e1cf3
Church-17 <matteo.chiesa.mc@gmail.com>
2023-11-24 11:02:32 +0100

    chtype struct

--- versione_thread/struct.h
+++ versione_thread/struct.h
@@ -1,7 +1,9 @@
+#include <ncurses.h>
 #define typestructdef(datatype) struct datatype; typedef struct datatype datatype
 
 // Define types & scruct prototypes
 typedef char* str;
+typedef chtype* chstr;
 typestructdef(List_str);
 typestructdef(List_attr);
 typestructdef(Dict_str_int);
