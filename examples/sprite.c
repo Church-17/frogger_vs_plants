f964868c37093a07e9a5218dd35f0e300e372852
Church-17 <matteo.chiesa.mc@gmail.com>
2023-11-28 23:06:39 +0100

    update sprite

--- examples/sprite.c
+++ examples/sprite.c
@@ -4,13 +4,8 @@
 
 #define mvaptrintw(y, x, attr, fstr...) attron(attr); mvprintw(y, x, fstr); attroff(attr)
 #define GREEN_BLACK COLOR_PAIR(1)
-#define BLACK_GREEN COLOR_PAIR(2)
-#define YELLOW_BLACK COLOR_PAIR(3)
-#define BLACK_YELLOW COLOR_PAIR(4)
-#define YELLOW_GREEN COLOR_PAIR(5)
-#define GREEN_YELLOW COLOR_PAIR(6)
-#define MAGENTA_BLACK COLOR_PAIR(7)
-#define MAGENTA_GREEN COLOR_PAIR(8)
+#define GREEN_YELLOW COLOR_PAIR(2)
+#define MAGENTA_GREEN COLOR_PAIR(3)
 
 
 int main() {
@@ -34,14 +29,8 @@ int main() {
     // Set needed color pairs (text, bg)
     init_pair(0, COLOR_WHITE, COLOR_BLACK);
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    init_pair(2, COLOR_BLACK, COLOR_GREEN);
-    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(4, COLOR_BLACK, COLOR_YELLOW);
-    init_pair(5, COLOR_YELLOW, COLOR_GREEN);
-    init_pair(6, COLOR_GREEN, COLOR_YELLOW);
-    init_pair(7, COLOR_MAGENTA, COLOR_BLACK);
-    init_pair(8, COLOR_MAGENTA, COLOR_GREEN);
-    init_pair(9, COLOR_BLACK, COLOR_WHITE);
+    init_pair(2, COLOR_GREEN, COLOR_YELLOW);
+    init_pair(3, COLOR_MAGENTA, COLOR_GREEN);
 
     int y = 1, x = 1;
 
@@ -56,7 +45,7 @@ int main() {
     mvaptrintw(y+1, x+7, GREEN_BLACK, "▄▀"); //
 
     mvaptrintw(y+2, x+2, GREEN_BLACK, "▄"); //
-    mvaptrintw(y+2, x+3, YELLOW_GREEN, " ▀▄▀");
+    mvaptrintw(y+2, x+3, GREEN_YELLOW, "█▄▀▄");
     mvaptrintw(y+2, x+7, GREEN_BLACK, "▄"); //
 
     mvaptrintw(y+3, x, GREEN_BLACK, "▀█▀"); //
@@ -64,7 +53,7 @@ int main() {
 
     getch();
 
-    printf("%d %d\n", LINES, COLS);
+    printf("%d %d ", LINES, COLS);
     endwin();
 
 }
