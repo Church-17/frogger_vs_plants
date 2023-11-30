93b24a6f68b982623ea359790784123df44916b3
Church-17 <matteo.chiesa.mc@gmail.com>
2023-11-30 10:38:14 +0100

    fix colors in background

--- examples/sprite_move.c
+++ examples/sprite_move.c
@@ -7,11 +7,7 @@
 
 #include <locale.h>
 #include <time.h>
-#include "../main.h"
-#include "../menu.h"
-#include "../game.h"
-#include "../str.h"
-#include "../utils.h"
+#include "../struct.h"
 
 #define FROG_X_DIM 10
 #define FROG_Y_DIM 4
@@ -22,9 +18,10 @@ void move_frog(WINDOW* win, int y, int x);
 #define GREEN_BLACK COLOR_PAIR(1)
 #define GREEN_YELLOW COLOR_PAIR(2)
 #define MAGENTA_GREEN COLOR_PAIR(3)
+#define GREEN_RED COLOR_PAIR(5)
+#define GREEN_BLUE COLOR_PAIR(4)
 
-
-
+#define LIM_RED_BLUE 17
 
 int main(void){
     setlocale(LC_ALL, ""); // Enable UTF-8 chars
@@ -49,22 +46,59 @@ int main(void){
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_YELLOW);
     init_pair(3, COLOR_MAGENTA, COLOR_GREEN);
+    init_pair(4, COLOR_GREEN, COLOR_BLUE);
+    init_pair(5, COLOR_GREEN, COLOR_RED);
 
-    int y = LINES-5, x = COLS/2;
+    int i, c, y = LINES/2, x = COLS/2, old_y, old_x;
+
+    for(i = 0; i < LIM_RED_BLUE; i++) {
+        mvhline(i, 0, ' ' | COLOR_PAIR(5), COLS);
+    }
+    for(i = LIM_RED_BLUE; i < LINES; i++) {
+        mvhline(i, 0, ' ' | COLOR_PAIR(4), COLS);
+    }
+
     move_frog(stdscr, y, x);
 
     while(1){
+        old_y = y, old_x = x;
+        c = getch();
+        switch(c) {
+            case KEY_UP:
+                if(y > 1) y -=FROG_Y_DIM;
+                if(y <= 1) y = 2;
+                break;
 
-        int c = getch();
-        switch (c) // controllo movimento
-        {
-            case KEY_UP: if (y > 1)  y -=3; if (y <= 1) y = 2; break;
-            case KEY_DOWN: if (y < LINES-FROG_Y_DIM)  y += 3; if (y >= LINES-FROG_Y_DIM) y = LINES-5; break;
-            case KEY_LEFT: if (x > 1)  x -= 3; if (x <= 1) x = 2; break;
-            case KEY_RIGHT: if (x < COLS-FROG_X_DIM)  x += 3; if (x >= COLS-FROG_X_DIM) x = COLS-11; break;
-            case (int)'q': werase(stdscr); endwin(); exit(0);
+            case KEY_DOWN:
+                if(y < LINES-FROG_Y_DIM) y += FROG_Y_DIM;
+                if(y >= LINES-FROG_Y_DIM) y = LINES-5;
+                break;
+
+            case KEY_LEFT:
+                if(x > 1) x -= 2;
+                if(x <= 1) x = 2;
+                break;
+
+            case KEY_RIGHT:
+                if(x < COLS-FROG_X_DIM) x += 2;
+                if(x >= COLS-FROG_X_DIM) x = COLS-11;
+                break;
+
+            case (int)'q':
+                werase(stdscr);
+                endwin();
+                exit(0);
+
             default: continue;
         }
+        for(i = old_y; i < old_y + FROG_Y_DIM; i++) {
+            if(i < LIM_RED_BLUE) {
+                mvhline(i, old_x, ' ' | COLOR_PAIR(5), FROG_X_DIM);
+            } else {
+                mvhline(i, old_x, ' ' | COLOR_PAIR(4), FROG_X_DIM);
+            }
+        }
+
         move_frog(stdscr, y, x);
 
     }
@@ -72,29 +106,37 @@ int main(void){
     return 0;
 }
 
-void move_frog(WINDOW* win, int y, int x)
-{
-    if ((y > 1 && y < LINES-FROG_Y_DIM) && (x > 1 && x < COLS-FROG_X_DIM))
-    {
-        werase(win);
-        wrefresh(win);
+void move_frog(WINDOW* win, int y, int x) {
+    int i;
+    attr_t temp[FROG_Y_DIM];
+    List_attr cols_bg;
+    cols_bg.list = temp;
+    cols_bg.len = FROG_Y_DIM;
 
-        mvaptrintw(y, x, GREEN_BLACK, "▄█"); //
-        mvaptrintw(y, x+3, MAGENTA_GREEN, "▀");
-        mvaptrintw(y, x+4, GREEN_YELLOW, "▌▐");
-        mvaptrintw(y, x+6, MAGENTA_GREEN, "▀");
-        mvaptrintw(y, x+8, GREEN_BLACK, "█▄"); //
-
-        mvaptrintw(y+1, x+1, GREEN_BLACK, "▀▄"); //
-        mvaptrintw(y+1, x+3, GREEN_YELLOW, " ▄  ");
-        mvaptrintw(y+1, x+7, GREEN_BLACK, "▄▀"); //
-
-        mvaptrintw(y+2, x+2, GREEN_BLACK, "▄"); //
-        mvaptrintw(y+2, x+3, GREEN_YELLOW, "█▄▀▄");
-        mvaptrintw(y+2, x+7, GREEN_BLACK, "▄"); //
-
-        mvaptrintw(y+3, x, GREEN_BLACK, "▀█▀"); //
-        mvaptrintw(y+3, x+7, GREEN_BLACK, "▀█▀"); //
+    for(i = 0; i < FROG_Y_DIM; i++) {
+        if(i + y < LIM_RED_BLUE) {
+            cols_bg.list[i] = GREEN_RED;
+        } else {
+            cols_bg.list[i] = GREEN_BLUE;
+        }
     }
+
+    mvaptrintw(y, x, cols_bg.list[0], "▄█"); //
+    mvaptrintw(y, x+3, MAGENTA_GREEN, "▀");
+    mvaptrintw(y, x+4, GREEN_YELLOW, "▌▐");
+    mvaptrintw(y, x+6, MAGENTA_GREEN, "▀");
+    mvaptrintw(y, x+8, cols_bg.list[0], "█▄"); //
+
+    mvaptrintw(y+1, x+1, cols_bg.list[1], "▀▄"); //
+    mvaptrintw(y+1, x+3, GREEN_YELLOW, " ▄  ");
+    mvaptrintw(y+1, x+7, cols_bg.list[1], "▄▀"); //
+
+    mvaptrintw(y+2, x+2, cols_bg.list[2], "▄"); //
+    mvaptrintw(y+2, x+3, GREEN_YELLOW, "█▄▀▄");
+    mvaptrintw(y+2, x+7, cols_bg.list[2], "▄"); //
+
+    mvaptrintw(y+3, x, cols_bg.list[3], "▀█▀"); //
+    mvaptrintw(y+3, x+7, cols_bg.list[3], "▀█▀"); //
+    
     return;
 }
