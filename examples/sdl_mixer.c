aa78776c8339f63a9ebb4cad648268942e1c3b52
Church-17 <matteo.chiesa.mc@gmail.com>
2023-12-11 12:03:59 +0100

    sdl

---
+++ examples/sdl_mixer.c
@@ -0,0 +1,55 @@
+// libsdl2-mixer-dev
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//The music that will be played
+Mix_Music *music = NULL;
+
+//The sound effects that will be used
+Mix_Chunk *scratch = NULL;
+
+void play_sound(Mix_Chunk* sound);
+
+int main(void) {
+    // Init SDL engine
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+        return 1;    
+    }
+    
+    //Init SDL_mixer
+    if(Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 128) == -1) {
+        return 1;    
+    }
+    
+    //Load the music
+    // music = Mix_LoadMUS( "beat.wav" );
+    
+    // Load effects
+    scratch = Mix_LoadWAV( "../audio/select_new.wav" );
+
+    while(getchar() != 'q') {
+        play_sound(scratch);
+    }
+
+    // Free effects
+    Mix_FreeChunk( scratch );
+    scratch = NULL;
+    
+    //Free the music
+    // Mix_FreeMusic( music );
+    
+    //Quit SDL_mixer
+    Mix_Quit();
+    
+    //Quit SDL
+    SDL_Quit();
+
+}
+
+void play_sound(Mix_Chunk* sound) {
+
+    Mix_PlayChannel(-1, sound, 0);
+
+}
