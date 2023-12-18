f520c85b3b96f1b71f94ca25fda8a841f8abdfae
Church-17 <matteo.chiesa.mc@gmail.com>
2023-12-18 10:18:06 +0100

    better time algorithm

--- versione_processi/time.c
+++ versione_processi/time.c
@@ -5,9 +5,6 @@
 #include "process.h"
 #include "time.h"
 
-// Define constant
-#define FLEX_INTERVAL 1
-
 void time_process(int pipe_write, int* other_params) {
     // Init vars
     time_t start = timestamp(), end = start, elapsed;
@@ -18,16 +15,13 @@ void time_process(int pipe_write, int* other_params) {
     // Loop for beat time
     while(TRUE) {
         elapsed = end - start; // Calc elapsed milliseconds
-        if(elapsed == MSEC_IN_SEC - FLEX_INTERVAL) elapsed++; // Give flexibility of an interval
-        if(elapsed >= MSEC_IN_SEC) { // If a seconds passed...
+        while(elapsed >= MSEC_IN_SEC) { // If a seconds passed...
             start = end; // Update start
             msg.y--;
             writer(pipe_write, &msg); // Write in pipe
             elapsed -= MSEC_IN_SEC; // Decrese elapsed of a second
         }
-        if(elapsed < MSEC_IN_SEC) { // If elasped is now less that a second
-            msleep(MSEC_IN_SEC - elapsed); // Wait for the milliseconds left to get to a second
-        }
+        msleep(MSEC_IN_SEC - elapsed); // Wait for the milliseconds left to get to a second
         end = timestamp(); // Update end
     }
 }
