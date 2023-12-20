615c965f8bf7a2fd336f061262bb33881d075534
Church-17 <matteo.chiesa.mc@gmail.com>
2023-12-20 09:19:46 +0100

    new files thread

---
+++ versione_thread/manche.c
@@ -0,0 +1,14 @@
+// Include libs
+#include "../main.h"
+#include "../menu.h"
+#include "../manche.h"
+#include "../utils.h"
+#include "../struct.h"
+#include "thread.h"
+#include "frog.h"
+#include "time.h"
+#include "croccodile.h"
+
+// Define constant
+#define RESIZE_TIME_THRESHOLD 100
+#define REFRESH_TIME_THRESHOLD 100
