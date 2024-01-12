2a2d20514aa919013e897ab9e8490f2959a9e480
Church-17 <matteo.chiesa.mc@gmail.com>
2024-01-12 16:00:06 +0100

    comments

--- music.c
+++ music.c
@@ -9,36 +9,47 @@
 #define MAX_VOLUME 32768
 
 void play_sound(int sound_id) {
+    // Init vars
     static const str sound[N_SOUND_EFFECTS] = {"dead_plant", "fire", "l_manche", "score", "select", "spawn_plant", "start_game", "w_manche"};
+    if(VOL_EFCT_SET == 0) { // If volume == 0 don't play anything
+        return;
+    }
     char cmd[LIM_STR_BUFF];
+
+    // Run mp3 player with scaled volume
     sprintf(cmd, "mpg123 -f%d ./audio/%s.mp3 2>/dev/null &", VOL_EFCT_SET*MAX_VOLUME/10, sound[sound_id]);
     system(cmd);
 }
 
 void play_music(int music_id) {
+    // Init vars
     static const str music[N_MUSICS] = {"best", "easy", "hard", "l_game", "medium", "menu", "w_game"};
     static const bool loop[N_MUSICS] = {FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE};
-    if(VOL_MUS_SET == 0) {
+    if(VOL_MUS_SET == 0) { // If volume == 0 don't play anything
         return;
     }
-    stop_music();
     char cmd[LIM_STR_BUFF];
-    if(loop[music_id]) {
+
+    stop_music(); // Stop music
+    if(loop[music_id]) { // If the music wanted need to be played in loop...
         sprintf(cmd, "mpg123 -f%d --loop -1 ./audio/%s.mp3 2>/dev/null &", VOL_MUS_SET*MAX_VOLUME/10, music[music_id]);
     } else {
         sprintf(cmd, "mpg123 -f%d ./audio/%s.mp3 2>/dev/null &", VOL_MUS_SET*MAX_VOLUME/10, music[music_id]);
     }
-    system(cmd);
+    system(cmd); // Run
 }
 
+// Stop all sounds
 void stop_music(void) {
     system("skill -SIGKILL -c mpg123");
 }
 
+// Pause all sounds
 void pause_music(void) {
     system("skill -SIGSTOP -c mpg123");
 }
 
+// Resume all paused sounds
 void resume_music(void) {
     system("skill -SIGCONT -c mpg123");
 }
